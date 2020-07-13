8ed04dda01365decf6de540ebc31e64300cd8543
yuill <yu10429@naver.com>
2020-07-13 22:27:53 +0900

    yock

---
+++ leejeawondoggy/src/leejeawondoggy/leejaewondoggy.java
@@ -0,0 +1,11 @@
+package leejeawondoggy;
+
+public class leejaewondoggy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Leejeawon");
+		System.out.println("doggy");
+	}
+
+}
