3da8a609b71caa7763955a120aaa8d2cbe6ec79e
yuill <yu10429@naver.com>
2020-07-14 09:12:59 +0900

    TestExam 추가

---
+++ 0714/src/TestExam.java
@@ -0,0 +1,13 @@
+
+public class TestExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 1; i<6; i++) {
+			System.out.print(i+"	");}
+		System.out.println("");
+		for(int i = 10; i>5; i--) {
+			System.out.print(i+"	");}
+}
+
+}
